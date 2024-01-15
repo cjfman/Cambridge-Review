ae96b4bdb7da23de662bd3ea2fdc02183790d49e
Charles Franklin <cjfman@gmail.com>
2024-01-14 20:51:12 -0500

    Place overquota lines behind quota line

--- scripts/plot_line_chart.py
+++ scripts/plot_line_chart.py
@@ -6,11 +6,12 @@ from typing import Sequence
 
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 import numpy as np
 
 import elections
 
-FILE = "/home/charles/Projects/cambridge_review/elections/csvs_sc/sc_election_2005.csv"
+FILE = "/home/charles/Projects/cambridge_review/elections/csvs_sc/sc_election_2003.csv"
 
 
 def nextOpenVPostition(
@@ -50,6 +51,7 @@ def main():
         color='black',
         linewidth=3,
     )
+    plt.text(election.num_rounds, election.quota, "- " + format(election.quota, ','), va='center', ha='left', weight='bold')
 
     ## Add vote lines
     line_labels = [(election.quota, 'Quota', { 'color': 'black', 'weight': 'bold' })]
@@ -63,15 +65,20 @@ def main():
         if not votes[-1]:
             ## Candidate lost
             p = plt.plot(np.array(range(1, round_count)), np.array(votes[:-1]), linewidth=1)
+            #plt.scatter(round_count-1, votes[-2], marker='x', color='red')
             plt.text(round_count-1, votes[-2], "x", va='center', ha='center', color='red', size=15)
         else:
             ## Candidate won
-            p = plt.plot(np.array(range(1, round_count + 1)), np.array(votes), linewidth=1)
-            color = p[-1].get_color()
+#            p = plt.plot(np.array(range(1, round_count + 1)), np.array(votes), linewidth=1)
+#            color = p[-1].get_color()
             ## Were they at or above quota?
             if round_count == 1 or votes[-2] > election.quota:
+                p = plt.plot(np.array(range(1, round_count + 1)), np.array(votes), linewidth=1, zorder=-1)
+                color = p[-1].get_color()
                 plt.scatter(round_count-1, votes[-2], marker='D', color=color, zorder=10000)
             else:
+                p = plt.plot(np.array(range(1, round_count + 1)), np.array(votes), linewidth=1)
+                color = p[-1].get_color()
                 plt.scatter(round_count, votes[-1], marker='D', color=color, zorder=10000)
 
         line_labels.append((votes[0], name, { 'color': p[-1].get_color() }))
@@ -85,16 +92,20 @@ def main():
         text_v_pos.append(v)
         #print(f"{name} {vpos} -> {v}")
 
-    ## Change Y limit
-    #ax.set_ylim(bottom=0, top=ax.get_ylim()[1])
-
     ## Add extra tick mark for quota
-    plt.yticks(list(plt.yticks()[0]) + [election.quota])
+    plt.yticks(list(plt.yticks()[0]) + [election.max_votes])
+
+    ## Legend
+    legend_elements = [
+        Line2D([0], [0], marker='D', color='black', label="Elected",  lw=0),
+        Line2D([0], [0], marker='x', color='red',   label="Defeated", lw=0, markersize=10),
+    ]
+    ax.legend(handles=legend_elements)
 
     ## Add labels to plot
-    plt.xlabel("Round")
-    plt.ylabel("Votes")
-    plt.title("Election")
+    plt.xlabel('Round',   weight='bold')
+    plt.ylabel('Votes',   weight='bold')
+    plt.title('School Comittee Election 2003', weight='bold')
     #plt.legend(loc='best', ncol=1)
     plt.tight_layout()
     plt.grid()
