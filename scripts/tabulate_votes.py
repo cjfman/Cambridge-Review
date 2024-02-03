4849545678e69876ac366b579bef1055621ec9a6
Charles Franklin <cjfman@gmail.com>
2024-02-03 11:26:12 -0500

    Reorganize tabulate_votes.py

--- scripts/tabulate_votes.py
+++ scripts/tabulate_votes.py
@@ -18,152 +18,153 @@ def parseArgs():
     return parser.parse_args()
 
 
-def main(args):
+def processResult(line, item):
+    match = re.match(r"(.+?)\s(\[\d-\d-\d(?:-\d)?\])", line)
+    if match:
+        action, vote = match.groups()
+        item['action'] = action
+        item['vote']   = vote
+        print(f"Found result: action:{action} vote:{vote}", file=sys.stderr)
+        return 'search'
+
+    ## Check for charter right
+    if 'action' not in item:
+        match = re.search(f"^CHARTER RIGHT EXERCISED BY (?:COUNCILLOR|VICE MAYOR|MAYOR) (?:(.+)(?: IN COUNCIL.*)?)", line)
+        if match:
+            name = match.groups()[0].replace(" IN COUNCIL", "").lower()
+            print(f"Charter righted by {name}", file=sys.stderr)
+            item['action'] = 'Charter Right'
+            item['charter_right'] = name
+            return 'search'
+
+    ## No match. Append line to action
+    if 'action' not in item:
+        item['action'] = line
+    else:
+        item['action'] += "\n" + line
+
+    return 'result'
+
+
+def processCouncillors(line, item, key):
+    councilors = line.replace(", ", ",")
+    print(f"Found {key}: {councilors}", file=sys.stderr)
+    if key not in item:
+        item[key] = councilors
+    else:
+        item[key] += councilors
+
+    if councilors[-1] == ',':
+        return key
+
+    return 'search'
+
+
+def tabulateVotes(lines):
     items = []
     item = None
     state = 'search'
-    with open(args.final_actions, 'r', encoding='utf8') as f:
-        for line in f:
-            line = line.strip()
-            if not line:
+    for line in lines:
+        print(f"State: {state}", file=sys.stderr)
+        line = line.strip()
+        if not line:
+            state = 'search'
+            continue
+
+        try:
+            ## Check for section header
+            match = re.search(r"^[ivxlcdm]+\.\s", line, re.IGNORECASE)
+            if match:
+                state = 'header'
+                print(f"Found section: {line}", file=sys.stderr)
+                if "COMMITTEE REPORTS" in line:
+                    break
+
+                if item is not None:
+                    items.append(item)
+                    item = None
                 continue
 
-            try:
-                ## Check for section header
-                match = re.search(r"^[ivxlcdm]+\.\s", line, re.IGNORECASE)
-                if match:
-                    state = 'header'
-                    print(f"Found section: {line}", file=sys.stderr)
-                    if "COMMITTEE REPORTS" in line:
-                        break
+            ## Check for agenda item description
+            match = re.search(r"^\d+\.\s", line)
+            if match:
+                print(f"Found agenda item description: {line[:20]}", file=sys.stderr)
+                state = 'description'
+                if item is not None:
+                    items.append(item)
+                    item = None
+                continue
 
-                    if item is not None:
-                        items.append(item)
-                        item = None
+            ## Check for item UID
+            #match = re.match(r"CMA|APP|ORD|COM|RES|POR|COF \d+ #\d+", title)
+            match = re.match(r"(?:CMA|APP|RES|POR) \d+ #\d+", line)
+            if match:
+                print(f"Found agenda item ID: {line}", file=sys.stderr)
+                if item is not None:
+                    items.append(item)
+
+                item = { 'uid': line }
+                state = 'item'
+                continue
+
+            ## Stop if there is no item
+            if item is None:
+                continue
+
+            ## Check for result
+            match = re.match(r"RESULT:\s*(.*)", line)
+            if match:
+                print("Start result", file=sys.stderr)
+                msg = match.groups()[0]
+                if msg:
+                    line = msg
+                    state = 'result'
+                else:
                     continue
 
-                ## Check for agenda item description
-                match = re.search(r"^\d+\.\s", line)
-                if match:
-                    print(f"Found agenda item description: {line[:20]}", file=sys.stderr)
-                    state = 'description'
-                    if item is not None:
-                        items.append(item)
-                        item = None
+            ## Check for YEAS, NAYS, PRESENT
+            match = re.match(r"(YEAS|NAYS|PRESENT):\s*(.*)", line)
+            if match:
+                key, msg = match.groups()
+                key = key.lower()
+                print(f"Start {key}", file=sys.stderr)
+                state = key
+                if msg:
+                    line = msg
+                else:
                     continue
 
-                ## Check for item UID
-                #match = re.match(r"CMA|APP|ORD|COM|RES|POR|COF \d+ #\d+", title)
-                match = re.match(r"(?:CMA|APP|RES|POR) \d+ #\d+", line)
-                if match:
-                    print(f"Found agenda item ID: {line}", file=sys.stderr)
-                    if item is not None:
-                        items.append(item)
+            ## Nothing matched. Must be a continuation of the previous state
+            if state == 'result':
+                state = processResult(line, item)
+            elif state == 'yeas':
+                state = processCouncillors(line, item, 'yeas')
+            elif state == 'nays':
+                state = processCouncillors(line, item, 'nays')
+            elif state == 'present':
+                state = processCouncillors(line, item, 'present')
 
-                    item = { 'uid': line }
-                    state = 'item'
-                    continue
+        except Exception as e:
+            print(f"Error when processing line: {line}", file=sys.stderr)
+            raise e
 
-                ## Stop if there is no item
-                if item is None:
-                    continue
+    ## Get the last item
+    if item is not None:
+        items.append(item)
+        item = None
 
-                ## Check for result
-                match = re.match(r"RESULT: (.+?)\s(\[\d-\d-\d(?:-\d)?\])", line)
-                if match:
-                    action, vote = match.groups()
-                    item['action'] = action
-                    item['vote']   = vote
-                    print(f"Found result: action:{action} vote:{vote}", file=sys.stderr)
-                    continue
-
-                ## Check for charter right
-                match = re.match(f"RESULT: CHARTER RIGHT EXERCISED BY (COUNCILLOR|VICE MAYOR|MAYOR) (.*) IN COUNCIL", line)
-                if match:
-                    name = match.groups()[0]
-                    print(f"Charter righted by {name}", file=sys.stderr)
-                    item['action'] = 'charter right'
-                    item['charter_right'] = name
-                    continue
-
-                ## Check for other result
-                match = re.match(f"RESULT: (.+)", line)
-                if match:
-                    result = match.groups()[0]
-                    print(f"Found result: {result}", file=sys.stderr)
-                    item['action'] = result
-
-                ## Check for YEAS
-                match = re.match(r"YEAS: (.+)", line)
-                if match:
-                    yeas = match.groups()[0].replace(", ", ",")
-                    print(f"Found yeas: {yeas}", file=sys.stderr)
-                    item['yeas'] = yeas
-                    if yeas[-1] == ',':
-                        state = 'yeas'
-                    else:
-                        state = 'search'
-
-                    continue
-
-                ## Check for NAYS
-                match = re.match(r"NAYS: (.+)", line)
-                if match:
-                    nays = match.groups()[0].replace(", ", ",")
-                    print(f"Found nays: {nays}", file=sys.stderr)
-                    item['nays'] = nays
-                    if nays[-1] == ',':
-                        state = 'nays'
-                    else:
-                        state = 'search'
-
-                    continue
-
-                ## Check for PRESENT
-                match = re.match(r"PRESENT: (.+)", line)
-                if match:
-                    present = match.groups()[0].replace(", ", ",")
-                    print(f"Found present: {present}", file=sys.stderr)
-                    item['present'] = present
-                    if present[-1] == ',':
-                        state = 'present'
-                    else:
-                        state = 'search'
-
-                    continue
-
-                ## Nothing matched. Must be a continuation of the previous state
-                if state == 'yeas':
-                    print(f"Found more yeas: {line}", file=sys.stderr)
-                    item['yeas'] += line.replace(", ", ",")
-                    if line[-1] != ',':
-                        state = 'search'
-                elif state == 'nays':
-                    print(f"Found more nays: {line}", file=sys.stderr)
-                    item['nays'] += line.replace(", ", ",")
-                    if line[-1] != ',':
-                        state = 'search'
-                elif state == 'present':
-                    print(f"Found more present: {line}", file=sys.stderr)
-                    item['present'] += line.replace(", ", ",")
-                    if line[-1] != ',':
-                        state = 'search'
-
-            except Exception as e:
-                print(f"Error when processing line: {line}", file=sys.stderr)
-                raise e
-
-        ## Get the last item
-        if item is not None:
-            items.append(item)
-            item = None
-
-        for item in items:
-            for key in columns:
-                if key not in item:
-                    item[key] = ""
+    for item in items:
+        for key in columns:
+            if key not in item:
+                item[key] = ""
 
     print(json.dumps(items, sort_keys=True, indent=4))
+
+
+def main(args):
+    with open(args.final_actions, 'r', encoding='utf8') as f:
+        tabulateVotes(f)
+
     return 0
 
 
