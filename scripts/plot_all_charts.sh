c8898420d8f133c80f8d2a1bf44089522b4ace5d
Charles Franklin <cjfman@gmail.com>
2024-02-05 21:36:51 -0500

    Modify some council sankey html charts

--- scripts/plot_all_charts.sh
+++ scripts/plot_all_charts.sh
@@ -8,14 +8,38 @@
 for Y in 01 03 05 07 09 11 13 15 17 19 21 23; do
     ./scripts/plot_sankey_chart.py --force-fixed-size --title "City Council Election 20$Y" \
         elections/csvs_cc/cc_election_20$Y.csv                                             \
-        charts/city_council/sankey/html/cc_election_sankey_fixed_size_20$Y.html
+        charts/city_council/sankey/html/cc_election_sankey_20$Y.html
 done
 
-for Y in ; do
+for Y in 01 03 05 07 13 15 19; do
     ./scripts/plot_sankey_chart.py --force-fixed-size --title "City Council Election 20$Y" \
         elections/csvs_cc/cc_election_20$Y.csv                                             \
         charts/city_council/sankey/html/cc_election_sankey_fixed_size_20$Y.html
 done
+
+./scripts/plot_sankey_chart.py --force-fixed-size --title "City Council Election 2009" \
+    --copyright "Charles J. Franklin" --copyright-tight                                \
+    elections/csvs_cc/cc_election_2009.csv                                             \
+    charts/city_council/sankey/html/cc_election_sankey_fixed_size_2009.html
+
+for Y in 11 17 21 23; do
+    ./scripts/plot_sankey_chart.py --force-fixed-size --title "City Council Election 20$Y" \
+        --width-ratio 500 --two-line-count 1000 \
+        elections/csvs_cc/cc_election_20$Y.csv                                             \
+        charts/city_council/sankey/html/cc_election_sankey_fixed_size_20$Y.html
+done
+
+for Y in 17 21 23; do
+    ./scripts/plot_sankey_chart.py --force-fixed-size --title "City Council Election 20$Y" \
+        --width-ratio 450 --two-line-count 1200 --copyright-tight                          \
+        elections/csvs_cc/cc_election_20$Y.csv                                             \
+        charts/city_council/sankey/html/cc_election_sankey_fixed_size_20$Y.html
+done
+
+./scripts/plot_sankey_chart.py --force-fixed-size --title "City Council Election 2023" \
+    --width-ratio 450 --two-line-count 1500 --copyright-tight                          \
+    elections/csvs_cc/cc_election_2023.csv                                             \
+    charts/city_council/sankey/html/cc_election_sankey_fixed_size_2023.html
 
 ## School Committee HTML
 for Y in 03 05 07 09 11 13 15 17 19 21 23; do
