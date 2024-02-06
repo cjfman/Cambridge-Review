fd387f2d43d900885e1eb470f6037240bf7ebf4b
Charles Franklin <cjfman@gmail.com>
2024-02-05 22:38:39 -0500

    Added council sankey pngs

--- scripts/plot_all_charts.sh
+++ scripts/plot_all_charts.sh
@@ -2,7 +2,7 @@
 
 ##########
 ## Sankey
-#########
+##########
 
 # City Council HTML
 for Y in 01 03 05 07 09 11 13 15 17 19 21 23; do
@@ -11,6 +11,7 @@ for Y in 01 03 05 07 09 11 13 15 17 19 21 23; do
         charts/city_council/sankey/html/cc_election_sankey_20$Y.html
 done
 
+# City Council HTML - Fixed Size
 for Y in 01 03 05 07 13 15 19; do
     ./scripts/plot_sankey_chart.py --force-fixed-size --title "City Council Election 20$Y" \
         elections/csvs_cc/cc_election_20$Y.csv                                             \
@@ -40,6 +41,43 @@ done
     --width-ratio 450 --two-line-count 1500 --copyright-tight                          \
     elections/csvs_cc/cc_election_2023.csv                                             \
     charts/city_council/sankey/html/cc_election_sankey_fixed_size_2023.html
+
+
+# City Council PNGs
+for Y in 07; do
+./scripts/plot_sankey_chart.py --title "City Council Election 20$Y" \
+    --width-ratio 200 --two-line-count 800 \
+    elections/csvs_cc/cc_election_20$Y.csv                          \
+    charts/city_council/sankey/png/cc_election_sankey_20$Y.png
+done
+
+for Y in 05 09 15; do
+./scripts/plot_sankey_chart.py --title "City Council Election 20$Y" \
+    --width-ratio 250 --two-line-count 800 \
+    elections/csvs_cc/cc_election_20$Y.csv                          \
+    charts/city_council/sankey/png/cc_election_sankey_20$Y.png
+done
+
+for Y in 01 03 05 11 13; do
+    ./scripts/plot_sankey_chart.py --title "City Council Election 20$Y" \
+        --width-ratio 300 --two-line-count 800 \
+        elections/csvs_cc/cc_election_20$Y.csv                          \
+        charts/city_council/sankey/png/cc_election_sankey_20$Y.png
+done
+
+for Y in 19 21 23; do
+    ./scripts/plot_sankey_chart.py --title "City Council Election 20$Y" \
+        --width-ratio 300 --two-line-count 800 \
+        elections/csvs_cc/cc_election_20$Y.csv                          \
+        charts/city_council/sankey/png/cc_election_sankey_20$Y.png
+done
+
+for Y in 17; do
+    ./scripts/plot_sankey_chart.py --title "City Council Election 20$Y" \
+        --width-ratio 350 --two-line-count 800 \
+        elections/csvs_cc/cc_election_20$Y.csv                          \
+        charts/city_council/sankey/png/cc_election_sankey_20$Y.png
+done
 
 ## School Committee HTML
 for Y in 03 05 07 09 11 13 15 17 19 21 23; do
