c7225cddec59a6ab8c4d02b31c1aaf42731451cd
Charles Franklin <cjfman@gmail.com>
2024-05-19 13:57:26 -0400

    Add ward data for 2017

--- scripts/draw_all_maps.sh
+++ scripts/draw_all_maps.sh
@@ -1,14 +1,25 @@
-#!/bin/bash
+#!/bin/bash -x
 
-./scripts/draw_wards_map.py --census-year 2010 --all --title "Election 2019 Precinct Map" \
-    elections/wards/council/wards_2019.csv maps/ward_all_2019.html
-./scripts/draw_wards_map.py --census-year 2010 --winners --title "Election 2019 Precinct Map" \
-    elections/wards/council/wards_2019.csv maps/ward_winners_2019.html
+if [[ ! -z $1 ]]; then
+    YEAR="$1"
+    CYEAR=2020
+    if [[ $YEAR < 2022 ]]; then
+        CYEA=2010
+    fi
+    echo "Generating election ward maps for election year $YEAR using the $CYEAR census"
+    ./scripts/draw_wards_map.py --census-year 2010 --all --title "Election $YEAR Precinct Map" \
+        elections/wards/council/wards_$YEAR.csv maps/ward_all_$YEAR.html
+    ./scripts/draw_wards_map.py --census-year 2010 --winners --title "Election $YEAR Precinct Map" \
+        elections/wards/council/wards_$YEAR.csv maps/ward_winners_$YEAR.html
+    exit;
+fi
 
-./scripts/draw_wards_map.py --census-year 2010 --all --title "Election 2021 Precinct Map" \
-    elections/wards/council/wards_2021.csv maps/ward_all_2021.html
-./scripts/draw_wards_map.py --census-year 2010 --winners --title "Election 2021 Precinct Map" \
-    elections/wards/council/wards_2021.csv maps/ward_winners_2021.html
+for YY in 15 17 19 21; do
+    ./scripts/draw_wards_map.py --census-year 2010 --all --title "Election 20$YY Precinct Map" \
+        elections/wards/council/wards_2019.csv maps/ward_all_2019.html
+    ./scripts/draw_wards_map.py --census-year 2010 --winners --title "Election 20$YY Precinct Map" \
+        elections/wards/council/wards_20$YY.csv maps/ward_winners_20$YY.html
+done
 
 ./scripts/draw_wards_map.py --census-year 2020 --all --title "Election 2023 Precinct Map" \
     elections/wards/council/wards_2023.csv maps/ward_all_2023.html
