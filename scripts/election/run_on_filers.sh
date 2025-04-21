7d52ebd81df49845b425a625e59225cf85c5408e
Charles Franklin <cjfman@gmail.com>
2025-04-20 22:02:28 -0400

    Even more financial stuff

---
+++ scripts/election/run_on_filers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILERS=$(perl -ne 'print "$1\n" if /"cpfId": (\d+)/;' candidate_data/filers.json)
+for FILER in $FILERS; do
+    CHART="charts/filers/reports/${FILER}_report_chart.html"
+    #./scripts/election/ocpf.py query-reports $FILER "candidate_data/reports/${FILER}_reports.json"
+    #./scripts/election/plot_finances.py --out "$CHART" --in-file "candidate_data/reports/${FILER}_reports.json" --copyright-tight --h-legend
+    #./scripts/add_no_cache.pl "$CHART"
+    if [[ ! -f "$CHART" ]]; then
+        cp charts/filers/reports/empty_report_chart.html "$CHART"
+    fi
+done
