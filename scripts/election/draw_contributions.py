9f8a511ce9d81ce33dc08764ce923ed7b18d2294
Charles Franklin <cjfman@gmail.com>
2025-09-20 13:44:15 -0400

    Don't plot state boundary

--- scripts/election/draw_contributions.py
+++ scripts/election/draw_contributions.py
@@ -523,7 +523,7 @@ def main(args):
     ## Make map
     m = folium.Map(location=[42.378, -71.11], zoom_start=14, tiles="Cartodb Positron")
     city_boundary = makeLayer(**CITY_BOUNDARY)
-    city_boundary = makeLayer(**STATE_BOUNDARY)
+    #city_boundary = makeLayer(**STATE_BOUNDARY)
     city_boundary.add_to(m)
 
     try:
